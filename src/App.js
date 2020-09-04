6349f47445a8543505c409b62835585eaf3299d2
Jy Kim <jy.kim@insytesys.com>
2020-09-03 17:56:30 -0700

    Add counter

--- src/App.js
+++ src/App.js
@@ -2,13 +2,39 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+export const OptionList = (props) => (
+  <select name={props.name} value={props.value} onChange={props.handleChange}>
+    
+  </select>
+)
+
 class App extends React.Component {
-  playAudio() {
+  constructor(props) {
+    super(props)
+    this.state = {value: 1}
+  }
+  playAudio = (event) => {
     const audioEl = document.getElementsByClassName("audio-element")[0]
-    audioEl.play()
+    console.log("Loop1 : " + this.state.value)
+    for (var i = 0; i < this.state.value; i++) {
+      setTimeout(() => {
+        audioEl.play();
+      }, i*1000);
+    }
   }
 
+  selectnum = (optionSelected) => {
+    this.setState({value: optionSelected.target.value})
+    console.log("Loop : " + this.state.value)
+  }
   render() {
+
+    const items = []
+
+    for ( var i = 0; i < 10; i++ ) {
+      items.push(<option key={i} value={i}>{i}</option>)
+    }
+
     return (
       <div
         style={{
@@ -17,9 +43,12 @@ class App extends React.Component {
           alignItems: "center"
         }}
       >
+        <select value={this.state.value} onChange={this.selectnum}>
+          {items}
+        </select> times
         <button onClick={this.playAudio}>
           <span>Play Yeet Joon!</span>
-        </button>
+        </button>&nbsp;
         <audio className="audio-element">
           <source src="yeet.mp3"></source>
         </audio>
