57fad8204af23de31c3c7ea0dcf5f157aa370278
Jy Kim <jy.kim@insytesys.com>
2020-09-08 12:13:32 -0700

    Every 10 times

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends React.Component {
     }
 
     let message = ""
-    if ( this.state.buttonClicked == 10 ) {
+    if ( this.state.buttonClicked % 10 == 9) {
       message = "     Work Harder"
     }
 
